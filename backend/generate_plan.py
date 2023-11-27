783b25d7162e252e5ad954fbdf564a6e03d9080d
rizkialldiansyah <rzzkalldi@gmail.com>
2023-11-27 16:10:13 +0700

    Maintenance infromation

--- backend/generate_plan.py
+++ backend/generate_plan.py
@@ -2,11 +2,11 @@ from random import uniform as rnd
 
 def meals_plan(number_of_meals):
     if number_of_meals==3:
-        return {'breakfast':0.35,'launch':0.40,'dinner':0.25}
+        return {'breakfast':0.35,'lunch':0.40,'dinner':0.25}
     elif number_of_meals==4:
-        return {'breakfast':0.30,'launch':0.40,'dinner':0.25,'morning snack':0.05}
+        return {'breakfast':0.30,'lunch':0.40,'dinner':0.25,'morning snack':0.05}
     else:
-        return {'breakfast':0.30,'launch':0.40,'dinner':0.20,'morning snack':0.05,'afternoon snack':0.05}
+        return {'breakfast':0.30,'lunch':0.40,'dinner':0.20,'morning snack':0.05,'afternoon snack':0.05}
     
 def recommend_meal(calories, macro, number_of_meals, prep_food_time):
     meals_calories_perc = meals_plan(number_of_meals)
@@ -18,7 +18,7 @@ def recommend_meal(calories, macro, number_of_meals, prep_food_time):
         meal_fat=meals_calories_perc[meal]*macro["fat"]
         if meal=='breakfast':        
             recommended_nutrition = {'nutritions': [meal_calories, meal_fat, rnd(0,4), rnd(0,30), rnd(0,400), meal_carbs, rnd(0,10), meal_protein, rnd(100,500)], 'prep_limit': prep_food_time}
-        elif meal=='launch':
+        elif meal=='lunch':
             recommended_nutrition = {'nutritions': [meal_calories, meal_fat, rnd(0,4), rnd(0,30), rnd(0,400), meal_carbs, rnd(0,10), meal_protein, rnd(100,500)], 'prep_limit': prep_food_time}
         elif meal=='dinner':
             recommended_nutrition = {'nutritions': [meal_calories, meal_fat, rnd(0,4), rnd(0,30), rnd(0,400), meal_carbs, rnd(0,10), meal_protein, rnd(100,500)], 'prep_limit': prep_food_time}
